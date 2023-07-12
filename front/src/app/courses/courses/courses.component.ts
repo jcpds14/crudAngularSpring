ce95839eec49cdcd8d2d52c5b37e15792a9962b8
Jean Carlos <jcpds14@gmail.com>
2023-07-12 13:35:50 -0300

    add Material table to list courses

--- front/src/app/courses/courses/courses.component.ts
+++ front/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/courses/model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end' }
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    // pode ser inicializado por aqui também
+    // this.courses = [];
+  }
+
+  ngOnInit(): void {
+
+  }
 }
