488d9947b9ff97aea1e2be05e40b67489d2d5e0c
Jean Carlos <jcpds14@gmail.com>
2023-07-12 15:37:43 -0300

    modify style.css

--- front/src/app/courses/courses/courses.component.ts
+++ front/src/app/courses/courses/courses.component.ts
@@ -9,7 +9,7 @@ import { Course } from '../model/courses/model/course';
 export class CoursesComponent implements OnInit {
 
   courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end' }
+    { _id: '1', name: 'Angular', category: 'Front-end' }
   ];
   displayedColumns = ['name', 'category'];
 
